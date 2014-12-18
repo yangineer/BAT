6d391f21f9bb7e2addd5336fe968d23dffb17fbb
Yang Wang <yang.wang@mail.utoronto.ca>
2014-12-18 16:56:17 -0500

    Added admin page as well as updated models

--- bat/attendance/admin.py
+++ bat/attendance/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from attendance.models import Job, Musician, Roster, Attendance, Uniform
 
 # Register your models here.
+
+@admin.register(Musician)
+class MusicianAdmin(admin.ModelAdmin):
+	list_display = ('__str__', 'instrument_section', 'is_on_strength', 'is_retired')
+	list_editable = ('instrument_section', 'is_on_strength') 
+	list_filter = ('is_retired', 'is_on_strength')
+
+@admin.register(Uniform)
+class UniformAdmin(admin.ModelAdmin):
+	list_display = ('__str__', 'name', 'headdress', 'tunic', 'kit')
+	list_editable = ('name', 'headdress', 'tunic', 'kit')
+admin.site.register(Job)
--- bat/attendance/models.py
+++ bat/attendance/models.py
@@ -5,16 +5,16 @@ from django.conf import settings
 class Musician(models.Model):
 	""" Represents a musician """
 	INSTRUMENT_SECTION_CHOICES = (
-		('LEAD', 'Leaders'),
-		('FLPC', 'Flutes & Piccolos'),
-		('CLAR', 'Clarinets'),
-		('OBBS', 'Oboes & Bassoons'),
-		('SAXS', 'Saxophones'),
-		('FRHN', 'French Horns'),
-		('TRPT', 'Trumpets'),
-		('TRBN', 'Trombones'),
-		('EUTB', 'Euphoniums & Tubas & String Basses'),
-		('PERC', 'Percussions'),
+		('0LEAD', 'Leaders'),
+		('1FLPC', 'Flutes & Piccolos'),
+		('2CLAR', 'Clarinets'),
+		('3OBBS', 'Oboes & Bassoons'),
+		('4SAXS', 'Saxophones'),
+		('5FRHN', 'French Horns'),
+		('6TRPT', 'Trumpets'),
+		('7TRBN', 'Trombones'),
+		('8EUTB', 'Euphoniums & Tubas & String Basses'),
+		('9PERC', 'Percussions'),
 	)
 	RANK_CHOICES = (
 		('00', 'Musn'),
@@ -30,25 +30,28 @@ class Musician(models.Model):
 		('90', 'Maj'),
 	)
 	rank = models.CharField(max_length=2, choices=RANK_CHOICES)
-	instrument_section = models.CharField(max_length=4, choices=INSTRUMENT_SECTION_CHOICES)
-	user = models.OneToOneField(settings.AUTH_USER_MODEL)
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	instrument_section = models.CharField(max_length=5, choices=INSTRUMENT_SECTION_CHOICES)
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, blank=True)
 	is_retired = models.BooleanField(default=False)
+	is_on_strength = models.BooleanField(default=False)
+
+	def __str__(self):
+		return '%s %s, %s' % (self.get_rank_display(), self.last_name, self.first_name)
 
 	class Meta:
-		ordering = ['-rank']
+		ordering = ['instrument_section', '-rank']
 
-class On_Str_Musician(Musician):
-	""" Represents an on-strength musician """
-	service_number = models.CharField(max_length=9)
+# class On_Str_Musician(Musician):
+# 	""" Represents an on-strength musician """
+# 	service_number = models.CharField(max_length=9, blank=True)
 
-class Attendance(models.Model):
-	""" Represents the attendance """
-	musicians_present = models.ManyToManyField(Musician, related_name='present')
-	musicians_absent = models.ManyToManyField(Musician, related_name='absent')
+# 	def on_str(self):
+# 		return True
 
-class Roster(models.Model):
-	""" Represents the booking roster """
-	musicians_booked = models.ManyToManyField(Musician)
+# 	class Meta:
+# 		verbose_name = 'On Strength Musician'
 
 class Uniform(models.Model):
 	""" Represents the different uniform/kit options """
@@ -72,17 +75,38 @@ class Uniform(models.Model):
 		('LY', 'Lyre'),
 		('ML', 'Music Stand & Lyre'),
 	)
-
+	name = models.CharField(max_length=30)
 	headdress = models.CharField(max_length=2, choices=HEADDRESS_CHOICES)
 	tunic = models.CharField(max_length=2, choices=TUNIC_CHOICES)
+	kit = models.CharField(max_length=2, choices=KIT_CHOICES)
+	def __str__(self):
+		return '%s: %s/%s/%s' % (self.name, self.get_headdress_display(), self.get_tunic_display(), self.get_kit_display())
 
 class Job(models.Model):
 	""" Represents an engagement job """
 	name = models.CharField(max_length=30)
 	start_date = models.DateField()
-	end_date = models.DateField()
-	call_time = models.TimeField()
-	location = models.TextField()
-	uniform = models.OneToOneField(Uniform)
-	roster = models.OneToOneField(Roster)
-	attendance = models.OneToOneField(Attendance)
+	end_date = models.DateField(blank=True, default=None)
+	call_time = models.TimeField(blank=True, null=True)
+	location = models.CharField(max_length=30)
+	uniform = models.ForeignKey(Uniform, null=True, default=None)
+
+	def __str__(self):
+		return '%s at %s on %s' % (self.name, self.location, self.start_date)
+
+	def save(self, *args, **kwargs):
+		""" Sets the end date to start date by default """
+		if not self.end_date:
+			self.end_date = self.start_date
+		super(Job, self).save(*args, **kwargs)
+
+class Attendance(models.Model):
+	""" Represents the attendance """
+	musicians_present = models.ManyToManyField(Musician, related_name='present')
+	musicians_absent = models.ManyToManyField(Musician, related_name='absent', null=True, default=None)
+	job = models.OneToOneField(Job, null=True, default=None)
+
+class Roster(models.Model):
+	""" Represents the booking roster """
+	musicians_booked = models.ManyToManyField(Musician)
+	job = models.OneToOneField(Job, null=True, default=None)
