a754d76cc05c926b9e8d12ec142ffbd8c67a6729
Yang Wang <yang.wang@mail.utoronto.ca>
2014-12-27 17:21:43 -0500

    Modified settings for deployment

--- bat/bat/settings.py
+++ bat/bat/settings.py
@@ -17,12 +17,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!814iy3er(2+s+*g#x3-4(e-602c2!bjh==5zu=1q*^4=8x(cb'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
