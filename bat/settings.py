ea83f13843b78b27351a905b820075c2c8c0c6b1
Yangineer <yang.wang@mail.utoronto.ca>
2014-12-27 20:09:55 -0500

    modified settings for databases

--- bat/settings.py
+++ bat/settings.py
@@ -56,19 +56,15 @@ ROOT_URLCONF = 'bat.urls'
 
 WSGI_APPLICATION = 'bat.wsgi.application'
 
-
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -100,4 +96,8 @@ STATICFILES_DIRS = (
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
-        
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES = ['default'] =  dj_database_url.config()        
